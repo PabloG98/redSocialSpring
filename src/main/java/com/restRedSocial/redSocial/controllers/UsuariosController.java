52b4fedc5a3f2bf9100def39450b9e6916eb92b9
pablo garcia <pablo.garcia@geo.com.gt>
2019-09-10 16:21:46 -0600

    second commit

--- src/main/java/com/restRedSocial/redSocial/controllers/UsuariosController.java
+++ src/main/java/com/restRedSocial/redSocial/controllers/UsuariosController.java
@@ -3,12 +3,24 @@ package com.restRedSocial.redSocial.controllers;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import com.restRedSocial.redSocial.DTO.JwtDTO;
+import com.restRedSocial.redSocial.DTO.Mensaje;
 import com.restRedSocial.redSocial.models.Usuarios;
+import com.restRedSocial.redSocial.security.JWT.*;
 import com.restRedSocial.redSocial.repositories.UsuarioRepository;
 import javax.validation.Valid;
 import java.util.List;
@@ -16,6 +28,13 @@ import java.util.List;
 @RestController
 @RequestMapping("/usuarios")
 public class UsuariosController {
+	
+	//@Autowired
+    //AuthenticationManager authenticationManager;
+	
+	@Autowired
+    JwtProvider jwtProvider;
+	
 	@Autowired
 	private UsuarioRepository repository;
 	
@@ -46,5 +65,19 @@ public class UsuariosController {
 	public void deleteUsuario(@PathVariable ObjectId id) {
 	   repository.delete(repository.findBy_id(id));
 	}
+	
+	//@PostMapping("/login")
+    //public ResponseEntity<JwtDTO> login(@Valid @RequestBody Usuarios loginUsuario, BindingResult bindingResult){
+        //if(bindingResult.hasErrors())
+            //return new ResponseEntity(new Mensaje("campos vacíos o email inválido"), HttpStatus.BAD_REQUEST);
+        //Authentication authentication = authenticationManager.authenticate(
+                //new UsernamePasswordAuthenticationToken(loginUsuario.getNombreUsuario(), loginUsuario.getContrasena())
+        //);
+        //SecurityContextHolder.getContext().setAuthentication(authentication);
+        //String jwt = jwtProvider.generateToken(authentication);
+        //UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        //JwtDTO jwtDTO = new JwtDTO(jwt, userDetails.getUsername(), userDetails.getAuthorities());
+        //return new ResponseEntity<JwtDTO>(jwtDTO, HttpStatus.OK);
+    //}
 
 }
--- src/main/java/com/restRedSocial/redSocial/models/Usuarios.java
+++ src/main/java/com/restRedSocial/redSocial/models/Usuarios.java
@@ -8,48 +8,70 @@ public class Usuarios {
 	public ObjectId _id;
 	public String nombre;
 	public String apellido;
+	public String nombreUsuario;
 	public String correo;
 	public String contrasena;
 	
 	public Usuarios() {}
-	
-	public Usuarios(ObjectId _id, String nombre, String apellido, String correo, String contrasena) {
+
+	public Usuarios(ObjectId _id, String nombre, String apellido, String nombreUsuario, String correo,
+			String contrasena) {
 		super();
 		this._id = _id;
 		this.nombre = nombre;
 		this.apellido = apellido;
+		this.nombreUsuario = nombreUsuario;
 		this.correo = correo;
 		this.contrasena = contrasena;
 	}
+
 	public ObjectId get_id() {
 		return _id;
 	}
+
 	public void set_id(ObjectId _id) {
 		this._id = _id;
 	}
+
 	public String getNombre() {
 		return nombre;
 	}
+
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	public String getApellido() {
 		return apellido;
 	}
+
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
 	public String getCorreo() {
 		return correo;
 	}
+
 	public void setCorreo(String correo) {
 		this.correo = correo;
 	}
+
 	public String getContrasena() {
 		return contrasena;
 	}
+
 	public void setContrasena(String contrasena) {
 		this.contrasena = contrasena;
 	}
+	
 	
 }
